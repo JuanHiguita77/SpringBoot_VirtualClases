bffe02f2d9809da1c8e1e756870ebdd766e291dc
Juan Yepes <juanferhiguita65@gmail.com>
2024-06-05 08:36:49 -0500

    feature/task-dev-05-ControllerStudent--Fix-user

--- src/main/java/com/riwi/virtualClasses/api/controllers/StudentController.java
+++ src/main/java/com/riwi/virtualClasses/api/controllers/StudentController.java
@@ -2,6 +2,7 @@ package com.riwi.virtualClasses.api.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.riwi.virtualClasses.api.dto.request.StudentReq;
@@ -20,32 +21,32 @@ public class StudentController {
     private IStudentService studentService;
 
     @GetMapping
-    public Page<StudentResp> getAll(@RequestParam int page, @RequestParam int size, @RequestParam(required = false) SortType sort, @RequestParam(required = false) String name) {
+    public ResponseEntity<Page<StudentResp>> getAll(@RequestParam int page, @RequestParam int size, @RequestParam(required = false) SortType sort, @RequestParam(required = false) String name) {
         if (name != null && !name.isEmpty()) {
-            return this.studentService.searchByName(name, page, size);
+            return ResponseEntity.ok(this.studentService.searchByName(name, page, size));
         }
         
-        return studentService.getAll(page, size, sort != null ? sort : SortType.NONE);
+        return ResponseEntity.ok(studentService.getAll(page, size, sort != null ? sort : SortType.NONE));
     }
 
     @GetMapping("/{id}")
-    public StudentResp get(@PathVariable Long id) {
-        return this.studentService.get(id);
+    public ResponseEntity<StudentResp> get(@PathVariable Long id) {
+        return ResponseEntity.ok(this.studentService.get(id));
     }
 
     @PostMapping
-    public StudentResp create(@RequestBody StudentReq studentReq) {
-        return this.studentService.create(studentReq);
+    public ResponseEntity<StudentResp> create(@RequestBody StudentReq studentReq) {
+        return ResponseEntity.ok(this.studentService.create(studentReq));
     }
 
     @PutMapping("/{id}")
-    public StudentResp update(@PathVariable Long id, @RequestBody StudentReq studentReq) {
-        return this.studentService.update(studentReq, id);
+    public ResponseEntity<StudentResp> update(@PathVariable Long id, @RequestBody StudentReq studentReq) {
+        return ResponseEntity.ok(this.studentService.update(studentReq, id));
     }
 
     @PatchMapping("/{id}/disable")
-    public StudentResp disable(@PathVariable Long id) {
+    public ResponseEntity<StudentResp> disable(@PathVariable Long id) {
         studentService.delete(id);
-        return this.studentService.get(id);
+        return ResponseEntity.ok(this.studentService.get(id));
     }
 }
