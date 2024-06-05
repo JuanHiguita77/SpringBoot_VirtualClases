1b4a39a8e8e049d6e7b9368d9ec636484b412f11
Juan Yepes <juanferhiguita65@gmail.com>
2024-06-05 06:28:02 -0500

    first commit

---
+++ src/main/java/com/riwi/virtualClasses/VirtualClassesApplication.java
@@ -0,0 +1,13 @@
+package com.riwi.virtualClasses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualClassesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualClassesApplication.class, args);
+	}
+
+}
