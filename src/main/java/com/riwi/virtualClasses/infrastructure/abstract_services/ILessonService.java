5dc7471c6208efb87e4e16b9d7290ed857a2018c
Juan Yepes <juanferhiguita65@gmail.com>
2024-06-05 09:40:48 -0500

    feature/task-dev-10-DisableLesson--Add-Lesson

--- src/main/java/com/riwi/virtualClasses/infrastructure/abstract_services/ILessonService.java
+++ src/main/java/com/riwi/virtualClasses/infrastructure/abstract_services/ILessonService.java
@@ -1,7 +1,5 @@
 package com.riwi.virtualClasses.infrastructure.abstract_services;
 
-import org.springframework.data.domain.Page;
-
 import com.riwi.virtualClasses.api.dto.request.LessonReq;
 import com.riwi.virtualClasses.api.dto.response.LessonResp;
 import com.riwi.virtualClasses.infrastructure.services.CrudService;
